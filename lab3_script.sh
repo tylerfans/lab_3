9127d463960f86fa6ab4d0b260cc18dc2d56787a
tylerfans <55064679+tylerfans@users.noreply.github.com>
2019-09-26 07:30:37 -0600

    Update lab3_script.sh

--- lab3_script.sh
+++ lab3_script.sh
@@ -3,7 +3,9 @@
 #Date: 9/20/19
 
 #Problem 1 Code
+#echo prints the statement and read gets the users input
 echo "Enter a file name"
+#this read line will be used to take the regex_practice file
 read filename
 echo "Enter a regular expression"
 read expression
@@ -11,8 +13,12 @@ read expression
 egrep "$expression" "$filename"
 #Problem 3 Code
 #303-458-9875
+#this gets all phone numbers. It looks for a 12 character string with numbers and hyphens
 egrep -c  '^[0-9-]{12}$' regex_practice.txt
 #Problem 4 Code
+#this gets the count of websites ending with .com
 egrep -c '.com$' regex_practice.txt
+#this gets the phone numbers starting with 303
 egrep -o '^303[0-9-]{9}$' regex_practice.txt
+#this moves all the emails with geocities.com  to the email_results.txt file
 egrep 'geocities.com$' regex_practice.txt >> email_results.txt
